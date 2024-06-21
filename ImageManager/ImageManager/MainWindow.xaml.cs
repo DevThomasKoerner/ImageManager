437ae7ff31276e316e91293c2db3dd4746ecd9f8
Thomas Koerner <koerner_a@arcor.de>
2024-06-21 14:52:13 +0200

    UI-Controls hinzugefügt.

--- ImageManager/ImageManager/MainWindow.xaml.cs
+++ ImageManager/ImageManager/MainWindow.xaml.cs
@@ -1,24 +1,30 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+
 
 namespace ImageManager
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow ()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+      InitializeComponent ();
+
     }
+
+    private void OnSourceButton_Clicked (object sender, RoutedEventArgs e)
+    {
+      
+    }
+
+    private void OnDestinationButton_Clicked (object sender, RoutedEventArgs e)
+    {
+     
+    }
+
+  }
 }
---
+++ ImageManager/ImageManager/ViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageManager
+{
+  public class ViewModel : INotifyPropertyChanged
+  {
+    private string _selectedDirectory;
+    public string SelectedDirectory
+    {
+      get => _selectedDirectory;
+      set
+      {
+        _selectedDirectory = value;
+        OnPropertyChanged ();
+      }
+    }
+
+    private string _imageSource;
+    public string ImageSource
+    {
+      get => _imageSource;
+      set
+      {
+        _imageSource = value;
+        OnPropertyChanged ();
+      }
+    }
+
+    public ViewModel ()
+    {
+      // Setze hier die Logik, um das Bild dynamisch zu wählen
+      // Beispiel:
+      ImageSource = "";
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged ([CallerMemberName] string propertyName = null)
+    {
+      PropertyChanged?.Invoke (this, new PropertyChangedEventArgs (propertyName));
+    }
+  }
+}
